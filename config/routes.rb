bd25fc1c49f1e8ebe810557fdde3a7179b82fc34
gittiggit <contactwithmahmud@gmail.com>
2016-01-15 10:46:13 +0600

    Commited after adding Stock model, stock_quote gem, users controller, non rest-full route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+  get 'users/my_portfolio'
+
   devise_for :users
   get "welcomes/index"
 	root "welcomes#index"
+  get "my_portfolio", to: "users#my_portfolio"
 end
